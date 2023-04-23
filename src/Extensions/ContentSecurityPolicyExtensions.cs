df176d7e8ad51bc03d9c2db8f36f8ccc9cfb55cb
Annabelle Horch <lodrial@gmail.com>
2023-04-22 21:40:42 -0500

    Enforce that the middleware can only be added to the pipeline once

--- src/Extensions/ContentSecurityPolicyExtensions.cs
+++ src/Extensions/ContentSecurityPolicyExtensions.cs
@@ -11,15 +11,25 @@ namespace AspNetCore.Hosting.ContentSecurityPolicies.Extensions
     public static class ContentSecurityPolicyExtensions
     {
         /// <summary>
+        /// Static field to enforce that the middleware is only activated once
+        /// </summary>
+        private static bool _middlewareAdded;
+
+        /// <summary>
         /// The HTTP Content-Security-Policy response header allows web site administrators to control resources the user agent is allowed to load for a given page. With a few exceptions, policies mostly involve specifying server origins and script endpoints. This helps guard against cross-site scripting attacks
         /// </summary>
         /// <param name="app"></param>
         /// <param name="builder"></param>
         /// <returns></returns>
         public static IApplicationBuilder UseContentSecurityPolicy(
-            [NotNull] this IApplicationBuilder app, 
+            [NotNull] this IApplicationBuilder app,
             [NotNull] Action<ContentSecurityPolicyBuilder> configurePolicy)
         {
+            if (_middlewareAdded)
+            {
+                throw new InvalidOperationException("Content Security Policy Middleware has already been added.");
+            }
+            _middlewareAdded = true;
             var builder = new ContentSecurityPolicyBuilder();
             configurePolicy(builder);
             return app.UseMiddleware<ContentSecurityPolicyMiddleware>(builder.BuildPolicy());
